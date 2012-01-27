1c54cd2142787c2fcefb94e1fc5e3ebfb39f185b
Andrew McWatters <mcwattersandrew@gmail.com>
2012-01-26 21:55:22 -0700

    Initial directory structure Added placeholder steamwebapi module

---
+++ steamwebapi.lua
@@ -0,0 +1,8 @@
+-------------------------------------------------------------------------------
+-- Scripted interfacing for the Steam Web API
+-- Steam Web Lua API
+-- Authors: Andrew McWatters
+-------------------------------------------------------------------------------
+local require = require
+
+module( "steamwebapi" )
