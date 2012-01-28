5556a29289bb64611d2081ac6852f06598c02475
Andrew McWatters <mcwattersandrew@gmail.com>
2012-01-28 15:55:09 -0700

    Fixed a parsing bug with url.escape()

--- steamwebapi/url.lua
+++ steamwebapi/url.lua
@@ -18,7 +18,7 @@ function escape (str)
 	str = string.gsub (str, "\n", "\r\n")
 	str = string.gsub (str, "([^0-9a-zA-Z ])", -- locale independent
 		function (c) return string.format ("%%%02X", string.byte(c)) end)
-	str = string.gsub (str, " ", "%20")
+	str = string.gsub (str, " ", "%%20")
 	return str
 end
 
